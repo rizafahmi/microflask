1fc0f91fbd6b87490688d9259ef0c0cc626d302d
Riza Fahmi <rizafahmi@gmail.com>
2016-09-15 22:24:22 +0700

    Finish our first test case. RED

---
+++ tests/app-test.py
@@ -0,0 +1,13 @@
+from app import app
+
+from unittest import TestCase, main
+
+class BasicTestCase(TestCase):
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Halo, Bandung!')
+
+if __name__ == '__main__':
+    main()
