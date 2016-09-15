26ffa6b563c5de0fcc19ecf971af260889d97f2a
Riza Fahmi <rizafahmi@gmail.com>
2016-09-15 23:48:15 +0700

    2nd RED: check for 404

--- tests/app-test.py
+++ tests/app-test.py
@@ -6,10 +6,15 @@ from unittest import TestCase, main
 
 class BasicTestCase(TestCase):
     def test_index(self):
-        tester = app.test_client(self)
+        tester = microflask.app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Halo, Bandung!')
+
+    def test_404(self):
+        tester = app.test_client(self)
+        response = tester.get('/404', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     main()
