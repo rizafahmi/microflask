372af3f451012566d9b584ee423407dc1fe76b9d
Riza Fahmi <rizafahmi@gmail.com>
2016-09-24 21:44:25 +0700

    Add more imports and remove some debugger

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, g, redirect
+from flask import (
+        Flask,
+        g,
+        redirect,
+        render_template,
+        flash,
+        url_for
+        )
 from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 import models
+import forms
 
 DEBUG = True
 PORT = 8000
@@ -16,6 +25,8 @@ app.config['MONGODB_SETTINGS'] = {'DB': MONGODB_NAME}
 # Setting up the DB
 db = MongoEngine(app)
 app.session_interface = MongoEngineSessionInterface(db)
+
+flask_bcrypt = Bcrypt(app)
 
 # Login Manager
 login_manager = LoginManager()
@@ -34,7 +45,31 @@ def load_user(userId):
     else:
         return None
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        username = form.username.data
+        email = form.email.data
+        password = flask_bcrypt.generate_password_hash(form.password.data)
+        user = models.User(
+                username=username,
+                email=email,
+                password=password)
+
+        try:
+            user.save()
+            return redirect(url_for('index'))
+        except:
+            flash("Unable to register.")
+    return render_template('register.html', form=form)
+
+@app.route('/')
+def index():
+    return 'Hey'
+
+
 if __name__ == '__main__':
     models.initialize()
-    import ipdb;ipdb.set_trace()
     app.run(debug=DEBUG, host=HOST, port=PORT)
